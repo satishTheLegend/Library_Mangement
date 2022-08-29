0b69a07ca58b20eddca64a250166ad6a32af5892
satish sharma <satishsh973@outlook.com>
2022-08-30 01:20:19 +0530

    conflicts resolved

--- Library_Mangement/Library_Mangement/Views/FlyoutView/FlyoutSubViews/CategoryView.xaml.cs
+++ Library_Mangement/Library_Mangement/Views/FlyoutView/FlyoutSubViews/CategoryView.xaml.cs
@@ -11,16 +11,6 @@ using Xamarin.Forms.Xaml;
 namespace Library_Mangement.Views.FlyoutView.FlyoutSubViews
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-<<<<<<< HEAD:Library_Mangement/Library_Mangement/Views/UserRegistration.xaml.cs
-    public partial class UserRegistration : ContentPage
-    {
-        #region Properites
-        private readonly UserRegistrationViewModel _vm;
-        #endregion
-
-        #region Constructor
-        public UserRegistration()
-=======
     public partial class CategoryView : ContentPage
     {
         #region Properties
@@ -29,14 +19,8 @@ namespace Library_Mangement.Views.FlyoutView.FlyoutSubViews
 
         #region Constructor
         public CategoryView()
->>>>>>> Updated Data with Categories:Library_Mangement/Library_Mangement/Views/FlyoutView/FlyoutSubViews/CategoryView.xaml.cs
         {
             _vm = new CategoryViewModel();
-            InitializeComponent();
-<<<<<<< HEAD:Library_Mangement/Library_Mangement/Views/UserRegistration.xaml.cs
-            _vm = new UserRegistrationViewModel(MainStackScroll, MainStack);
-=======
->>>>>>> Updated Data with Categories:Library_Mangement/Library_Mangement/Views/FlyoutView/FlyoutSubViews/CategoryView.xaml.cs
             BindingContext = _vm;
         }
         #endregion
@@ -44,8 +28,7 @@ namespace Library_Mangement.Views.FlyoutView.FlyoutSubViews
         #region Override Methods
         protected async override void OnAppearing()
         {
-<<<<<<< HEAD:Library_Mangement/Library_Mangement/Views/UserRegistration.xaml.cs
-            await _vm.LoadData();
+            await _vm.ExploreBooks();
             base.OnAppearing();
         }
         #endregion
@@ -53,12 +36,5 @@ namespace Library_Mangement.Views.FlyoutView.FlyoutSubViews
         #region Event Handlers
 
         #endregion
-
-=======
-            await _vm.ExploreBooks();
-            base.OnAppearing();
-        }
-        #endregion
->>>>>>> Updated Data with Categories:Library_Mangement/Library_Mangement/Views/FlyoutView/FlyoutSubViews/CategoryView.xaml.cs
     }
 }
--- Library_Mangement/Library_Mangement/Views/UserRegistration.xaml.cs
+++ Library_Mangement/Library_Mangement/Views/UserRegistration.xaml.cs
@@ -8,10 +8,9 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Library_Mangement.Views.FlyoutView.FlyoutSubViews
+namespace Library_Mangement.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-<<<<<<< HEAD:Library_Mangement/Library_Mangement/Views/UserRegistration.xaml.cs
     public partial class UserRegistration : ContentPage
     {
         #region Properites
@@ -20,23 +19,9 @@ namespace Library_Mangement.Views.FlyoutView.FlyoutSubViews
 
         #region Constructor
         public UserRegistration()
-=======
-    public partial class CategoryView : ContentPage
-    {
-        #region Properties
-        public readonly CategoryViewModel _vm;
-        #endregion
-
-        #region Constructor
-        public CategoryView()
->>>>>>> Updated Data with Categories:Library_Mangement/Library_Mangement/Views/FlyoutView/FlyoutSubViews/CategoryView.xaml.cs
         {
-            _vm = new CategoryViewModel();
             InitializeComponent();
-<<<<<<< HEAD:Library_Mangement/Library_Mangement/Views/UserRegistration.xaml.cs
             _vm = new UserRegistrationViewModel(MainStackScroll, MainStack);
-=======
->>>>>>> Updated Data with Categories:Library_Mangement/Library_Mangement/Views/FlyoutView/FlyoutSubViews/CategoryView.xaml.cs
             BindingContext = _vm;
         }
         #endregion
@@ -44,21 +29,9 @@ namespace Library_Mangement.Views.FlyoutView.FlyoutSubViews
         #region Override Methods
         protected async override void OnAppearing()
         {
-<<<<<<< HEAD:Library_Mangement/Library_Mangement/Views/UserRegistration.xaml.cs
             await _vm.LoadData();
             base.OnAppearing();
         }
         #endregion
-
-        #region Event Handlers
-
-        #endregion
-
-=======
-            await _vm.ExploreBooks();
-            base.OnAppearing();
-        }
-        #endregion
->>>>>>> Updated Data with Categories:Library_Mangement/Library_Mangement/Views/FlyoutView/FlyoutSubViews/CategoryView.xaml.cs
     }
 }
